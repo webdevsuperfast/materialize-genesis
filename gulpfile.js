926577c1b565e4f81b2a7c1b9d8da8f2f2de2bfd
Rotsen Mark Acob <acob.rotsen@hotmail.com>
2016-12-18 11:04:57 +0800

    Code cleanup

--- gulpfile.js
+++ gulpfile.js
@@ -1,98 +1,85 @@
 var gulp = require('gulp'),
     sass = require('gulp-sass'),
     autoprefixer = require('gulp-autoprefixer'),
+    minifycss = require('gulp-clean-css'),
     jshint = require('gulp-jshint'),
     uglify = require('gulp-uglify'),
     rename = require('gulp-rename'),
     concat = require('gulp-concat'),
     notify = require('gulp-notify'),
     cache = require('gulp-cache'),
+    prettify = require('gulp-jsbeautifier'),
     vinylpaths = require('vinyl-paths'),
-    cleancss = require('gulp-clean-css'),
-    cmq = require('gulp-combine-media-queries'),
-    concatcss = require('gulp-concat-css'),
+    cmq = require('gulp-combine-mq'),
+    merge = require('merge-stream'),
+    foreach = require('gulp-flatmap'),
+    changed = require('gulp-changed'),
     del = require('del');
 
 // CSS
-gulp.task('source:css', function(){
-    return gulp.src([
-            'stylesheet/source/*.scss',
-            'sass/app.scss'
-        ])
-        .pipe(sass.sync().on('error', sass.logError))
-        .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
-        .pipe(gulp.dest('temp/css'))
-        .pipe(rename({suffix: '.min'}))
-        .pipe(cleancss())
-        .pipe(gulp.dest('stylesheet'))
-        .pipe(notify({ message: 'Source styles task complete' }));
-} );
-
-gulp.task('vendor:css', function(){
-    return gulp.src([
-        'bower_components/normalize-css/normalize.css',
-        'stylesheet/vendor/*.css'
+gulp.task('styles', function(){
+    var cssStream = gulp.src([
+        // 'bower_components/normalize-css/normalize.css'
     ])
+    .pipe(concat('normalize.css'));
+
+    var sassStream = gulp.src('sass/app.scss')
+        .pipe(sass.sync().on('error', sass.logError))
+        .pipe(concat('app.scss'))
+    
+    var mergeStream = merge(sassStream, cssStream)
+        .pipe(concat('app.css'))
         .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
-        .pipe(concatcss('vendor.css'))
+        .pipe(cmq())
         .pipe(gulp.dest('temp/css'))
-        .pipe(rename({suffix: '.min'}))
-        .pipe(cleancss())
+        .pipe(rename('app.css'))
+        .pipe(prettify())
         .pipe(gulp.dest('stylesheet'))
-        .pipe(notify({ message: 'Vendor styles task complete' }));
-} );
+        .pipe(notify({ message: 'Styles task complete' }));
+    
+    return mergeStream;
+});
 
 // JSHint
 gulp.task('lint', function(){
-    return gulp.src('js/source/*.js')
+    return gulp.src('assets/js/source/*.js')
         .pipe(jshint('.jshintrc'))
         .pipe(jshint.reporter('default'))
 });
 
-// Theme JS
-gulp.task('source:js', function() {
+// Scripts
+gulp.task('scripts', function() {
     return gulp.src([
-        'js/source/*.js'
+        'js/source/*.js',
+        'bower_components/Materialize/dist/js/materialize.js'
     ])
-    .pipe(concat('app.js'))
-    .pipe(gulp.dest('temp/js'))
-    .pipe(rename({suffix: '.min'}))
-    .pipe(uglify())
+    .pipe(changed('js'))
+    .pipe(foreach(function(stream, file){
+        return stream
+            .pipe(uglify())
+            .pipe(rename({suffix: '.min'}))
+            .pipe(gulp.dest('temp/js'))
+    }))
     .pipe(gulp.dest('js'))
-    .pipe(notify({ message: 'Source scripts task complete' }));
+    .pipe(notify({ message: 'Scripts task complete' }));
 });
 
-// Vendor JS
-gulp.task('vendor:js', function(){
-    return gulp.src([
-        'bower_components/Materialize/dist/js/materialize.js',
-        'js/vendor/*.js'
-    ])
-    .pipe(concat('vendor.js'))
-    .pipe(gulp.dest('temp/js'))
-    .pipe(rename({suffix: '.min'}))
-    .pipe(uglify())
-    .pipe(gulp.dest('js'))
-    .pipe(notify({ message: 'Vendor scripts task complete' }));
-});
-
-// Clean temp folder
-gulp.task('clean:temp', function(){
+// Clean
+gulp.task('clean', function(cb) {
     return gulp.src('temp/*')
     .pipe(vinylpaths(del))
 });
 
 // Default task
-gulp.task('default', ['clean:temp'], function() {
-    gulp.start('source:css', 'vendor:css', 'lint', 'source:js', 'vendor:js', 'watch');
+gulp.task('default', ['clean'], function() {
+    gulp.start('styles', 'lint', 'scripts', 'watch');
 });
 
 // Watch
 gulp.task('watch', function() {
     // Watch .scss files
-    gulp.watch(['sass/*.scss', 'sass/**/*.scss'], ['source:css']);
+    gulp.watch(['sass/*.scss', 'sass/**/*.scss'], ['styles']);
 
     // Watch .js files
-    gulp.watch(['js/vendor/*.js'], ['vendor:js']);
-    gulp.watch(['js/source/*.js'], ['source:js']);
+    gulp.watch(['js/vendor/*.js', 'js/source/*.js'], ['scripts']);
 });
