9b482d28753ee4440a06c7e76ac1da505bab76a4
Rotsen Mark Acob <acob.rotsen@hotmail.com>
2017-01-06 12:17:44 +0800

    Code changes

--- gulpfile.js
+++ gulpfile.js
@@ -29,7 +29,7 @@ gulp.task('styles', function(){
     
     var mergeStream = merge(sassStream, cssStream)
         .pipe(concat('app.css'))
-        .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+        .pipe(autoprefixer('last 2 version'))
         .pipe(cmq())
         .pipe(gulp.dest('temp/css'))
         .pipe(rename('app.css'))
