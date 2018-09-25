666cc1dfb64ccda8c51b39480027cdbf108ec305
Rotsen Mark Acob <acob.rotsen@hotmail.com>
2018-09-25 10:32:55 +0800

    Use gulp-wp-toolkit

--- gulpfile.js
+++ gulpfile.js
@@ -1,80 +1,53 @@
+'use strict';
+
 var gulp = require('gulp'),
-    sass = require('gulp-sass'),
-    autoprefixer = require('gulp-autoprefixer'),
-    minifycss = require('gulp-clean-css'),
-    jshint = require('gulp-jshint'),
-    uglify = require('gulp-uglify'),
-    rename = require('gulp-rename'),
-    concat = require('gulp-concat'),
-    notify = require('gulp-notify'),
-    cache = require('gulp-cache'),
-    prettify = require('gulp-jsbeautifier'),
-    vinylpaths = require('vinyl-paths'),
-    cmq = require('gulp-combine-mq'),
-    merge = require('merge-stream'),
-    foreach = require('gulp-flatmap'),
-    changed = require('gulp-changed'),
-    del = require('del');
+    pkg = require('./package.json'),
+    toolkit = require('gulp-wp-toolkit');
 
-// CSS
-gulp.task('styles', function(){
-    var sassStream = gulp.src('assets/scss/app.scss')
-        .pipe(sass.sync().on('error', sass.logError))
-        .pipe(concat('app.scss'))
-    
-    var mergeStream = merge(sassStream)
-        .pipe(concat('app.css'))
-        .pipe(autoprefixer('last 2 version'))
-        .pipe(cmq())
-        .pipe(gulp.dest('temp/css'))
-        .pipe(rename('app.css'))
-        .pipe(minifycss({level: {1: {specialComments: 0 }}}))
-        .pipe(gulp.dest('assets/css'))
-        .pipe(notify({ message: 'Styles task complete' }));
-    
-    return mergeStream;
+toolkit.extendConfig({
+    theme: {
+        name: 'Materialize Genesis',
+        homepage: pkg.homepage,
+        description: pkg.description,
+        author: pkg.author,
+        version: pkg.version,
+        license: pkg.license,
+        textdomain: pkg.name
+    },
+    css: {
+        basefontsize: 16, // Used by postcss-pxtorem.
+		cssnano: {
+			discardComments: {
+				removeAll: true
+			},
+			zindex: false,
+		},
+        remreplace: false, // Used by postcss-pxtorem.
+        remmediaquery: true, // Used by postcss-pxtorem.
+		scss: {
+			'app': {
+				src: 'develop/scss/app.scss',
+				dest: 'assets/css/',
+				outputStyle: 'compressed',
+			},
+		},
+    },
+    js: {
+        'app': [
+            'develop/js/source/*.js'
+        ],
+        'materialize': [
+            'node_modules/materialize-css/dist/js/materialize.js'
+        ]
+    },
+    dest: {
+        js: 'assets/js/',
+        css: 'assets/css/'
+    },
+    server: {
+        proxy: 'wordpress.test',
+        online: true
+    }
 });
 
-// JSHint
-gulp.task('lint', function(){
-    return gulp.src('assets/js/source/*.js')
-        .pipe(jshint('.jshintrc'))
-        .pipe(jshint.reporter('default'))
-});
-
-// Scripts
-gulp.task('scripts', function() {
-    return gulp.src([
-        'assets/js/source/*.js',
-        'node_modules/materialize-css/dist/js/materialize.js'
-    ])
-    .pipe(changed('js'))
-    .pipe(foreach(function(stream, file){
-        return stream
-            .pipe(uglify())
-            .pipe(rename({suffix: '.min'}))
-            .pipe(gulp.dest('temp/js'))
-    }))
-    .pipe(gulp.dest('assets/js'))
-    .pipe(notify({ message: 'Scripts task complete' }));
-});
-
-// Clean
-gulp.task('clean', function(cb) {
-    return gulp.src('temp/*')
-    .pipe(vinylpaths(del))
-});
-
-// Default task
-gulp.task('default', ['clean'], function() {
-    gulp.start('styles', 'lint', 'scripts', 'watch');
-});
-
-// Watch
-gulp.task('watch', function() {
-    // Watch .scss files
-    gulp.watch(['assets/scss/*.scss', 'assets/scss/**/*.scss'], ['styles']);
-
-    // Watch .js files
-    gulp.watch(['assets/js/vendor/*.js', 'assets/js/source/*.js'], ['scripts']);
-});
+toolkit.extendTasks(gulp, /* Gulp task overrides. */);
